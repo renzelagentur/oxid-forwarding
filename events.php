b4b180a54bc7ec9c5a5a1414d849a29b3aba9247
Mathis Schuelingkamp <math@vkf-renzel.de>
2015-03-17 13:18:07 +0100

    Fixed events.php and metadata.php

--- events.php
+++ events.php
@@ -17,8 +17,7 @@ class raforwardingevents
     public static function onActivate()
     {
         $oDB = oxDb::getDb();
-        $oDB->execute(
-            'CREATE TABLE IF NOT EXISTS raforwarding (
+        $oDB->execute('CREATE TABLE IF NOT EXISTS raforwarding (
             `OXID` char(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
             `TITLE` varchar(255) NOT NULL,
             `ORIGIN` varchar(255) NOT NULL,
@@ -27,7 +26,6 @@ class raforwardingevents
             `OXSHOPID` int(11) NOT NULL,
             `OXPARENTID` char(32) DEFAULT NULL,
             PRIMARY KEY (`OXID`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8'
-        );
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8');
     }
 }
